a6016b0e149c32507d78c4f4f5d617abb507b32b
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2024-01-13 09:12:45 -0500

    11 Solution.

--- Blind 75/0011 - Container With Most Water/solution.py
+++ Blind 75/0011 - Container With Most Water/solution.py
@@ -1,8 +1,31 @@
 class Solution:
-    # Time Complexity:  TBD
-    # Space Complexity: TBD
+    # Time Complexity:  O(n) - iterating through given array once.
+    # Space Complexity: O(1) - no extra data structures required.
     def maxArea(self, height: list[int]) -> int:
-        pass
+        # Brute force implementation.
+        # maxArea = 0
+
+        # for l in range(len(height)):
+        #     for r in range(l + 1, len(height)):
+        #         currentArea = (r - l) * min(height[l], height[r])
+        #         maxArea = max(maxArea, currentArea)
+
+        # return maxArea
+
+        # Optimized implementation.
+        maxArea = 0
+        l, r = 0, len(height) - 1
+
+        while l < r:
+            currentArea = (r - l) * min(height[l], height[r])
+            maxArea = max(maxArea, currentArea)
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return maxArea
 
 
 if __name__ == '__main__':
