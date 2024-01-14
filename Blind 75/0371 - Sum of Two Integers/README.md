ad289b825d789259f589ecf232e4a7f2b44e6199
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2024-01-14 04:44:03 -0500

    371 Solution.

--- Blind 75/0371 - Sum of Two Integers/README.md
+++ Blind 75/0371 - Sum of Two Integers/README.md
@@ -1,3 +1,9 @@
 # Sum of Two Integers
 
 Given two integers `a` and `b`, return _the sum of the two integers without using the operators_ `+` _and_ `-`.
+
+---
+
+References:
+
+1. [Sum Of Two Integers - Leetcode 371- Blind 75 Explained - Binary - Python](https://youtu.be/_pUidg9gQyA?si=A1jV5PlW3PBqUkJA)
--- Blind 75/0371 - Sum of Two Integers/solution.py
+++ Blind 75/0371 - Sum of Two Integers/solution.py
@@ -1,8 +1,16 @@
 class Solution:
-    # Time Complexity:  TBD
-    # Space Complexity: TBD
+    # Time Complexity:  O(1) - using constant time logical operators XOR and AND.
+    # Space Complexity: O(1) - using constant time logical operators XOR and AND.
     def getSum(self, a: int, b: int) -> int:
-        pass
+        # Need for Python due to how Python represents size in Bits (much larger).
+        bitShortener = 0xffffffff
+
+        while (b & bitShortener) != 0:
+            carry = (a & b) << 1
+            a = (a ^ b)
+            b = carry
+
+        return (a & bitShortener)
 
 
 if __name__ == '__main__':
@@ -17,5 +25,8 @@ if __name__ == '__main__':
 
         def test_example_2(self):
             self.assertEqual(f(2, 3), 5)
+
+        def test_example_3(self):
+            self.assertEqual(f(9, 11), 20)
 
     unittest.main()
