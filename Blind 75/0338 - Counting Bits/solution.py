f3728ee7127c0d0b78b7d6ac4be5d29337e9364a
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2024-01-14 05:32:05 -0500

    338 Solution.

--- Blind 75/0338 - Counting Bits/solution.py
+++ Blind 75/0338 - Counting Bits/solution.py
@@ -1,8 +1,17 @@
 class Solution:
-    # Time Complexity:  TBD
-    # Space Complexity: TBD
+    # Time Complexity:  O(n) - iterating n times.
+    # Space Complexity: O(n) - iterating n times.
     def countBits(self, n: int) -> list[int]:
-        pass
+        ans = [0] * (n + 1)
+        offset = 1
+
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+
+            ans[i] = 1 + ans[i - offset]
+
+        return ans
 
 
 if __name__ == '__main__':
