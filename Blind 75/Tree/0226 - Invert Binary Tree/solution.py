215e8b441de9d22ecb9b6e9bb8f70fef22f733f7
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2024-01-15 08:20:18 -0500

    .

--- Blind 75/Tree/0226 - Invert Binary Tree/solution.py
+++ Blind 75/Tree/0226 - Invert Binary Tree/solution.py
@@ -1,12 +1,13 @@
 from typing import Optional
+from ....Helpers.Python.binary_tree import TreeNode, getLevelOrder
 
 
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 
 # Time Complexity:  TBD
@@ -29,46 +30,46 @@ class Solution:
         return root
 
 
-# Function to get level order traversal of tree.
-def getLevelOrder(root: Optional[TreeNode]) -> list[int]:
-    if root and root.val == 0 and root.left is None and root.right is None:
-        return []
+# # Function to get level order traversal of tree.
+# def getLevelOrder(root: Optional[TreeNode]) -> list[int]:
+#     if root and root.val == 0 and root.left is None and root.right is None:
+#         return []
 
-    while not root:
-        return []
+#     while not root:
+#         return []
 
-    queue = [root]
-    result = []
+#     queue = [root]
+#     result = []
 
-    while queue:
-        level_nodes = []
-        temp = []
-        for node in queue:
-            level_nodes.append(node.val)
-            if node.left:
-                temp.append(node.left)
-            if node.right:
-                temp.append(node.right)
-        queue = temp
-        result.append(level_nodes)
+#     while queue:
+#         level_nodes = []
+#         temp = []
+#         for node in queue:
+#             level_nodes.append(node.val)
+#             if node.left:
+#                 temp.append(node.left)
+#             if node.right:
+#                 temp.append(node.right)
+#         queue = temp
+#         result.append(level_nodes)
 
-    return [item for items in result for item in items]
+#     return [item for items in result for item in items]
 
 
-# Compute the height of a tree - the number of nodes along the longest path from the root node down to the farthest leaf node.
-def height(node: Optional[TreeNode]) -> int:
-    if node is None:
-        return 0
-    else:
-        # Compute the height of each subtree.
-        leftHeight = height(node.left)
-        rightHeight = height(node.right)
+# # Compute the height of a tree - the number of nodes along the longest path from the root node down to the farthest leaf node.
+# def getHeight(node: Optional[TreeNode]) -> int:
+#     if node is None:
+#         return 0
+#     else:
+#         # Compute the height of each subtree.
+#         leftHeight = height(node.left)
+#         rightHeight = height(node.right)
 
-        # Use the larger one.
-        if leftHeight > rightHeight:
-            return leftHeight + 1
-        else:
-            return rightHeight + 1
+#         # Use the larger one.
+#         if leftHeight > rightHeight:
+#             return leftHeight + 1
+#         else:
+#             return rightHeight + 1
 
 
 if __name__ == '__main__':
