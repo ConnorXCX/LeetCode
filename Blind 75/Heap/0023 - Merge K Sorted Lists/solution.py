744518f7c99fc7f5bff6ead4f3cd5ece8db9b165
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2024-01-15 13:55:06 -0500

    .

--- Blind 75/Heap/0023 - Merge K Sorted Lists/solution.py
+++ Blind 75/Heap/0023 - Merge K Sorted Lists/solution.py
@@ -23,32 +23,23 @@ if __name__ == '__main__':
     class Test(unittest.TestCase):
 
         def test_example_1(self):
-            self.assertEqual(f(), None)
+            # The linked-lists are:
+            # [
+            # 1->4->5,
+            # 1->3->4,
+            # 2->6
+            # ]
+            # merging them into one sorted list:
+            # 1->1->2->3->4->4->5->6
+            self.assertEqual(f([ListNode(1, ListNode(4, ListNode(5))),
+                                ListNode(1, ListNode(3, ListNode(4))),
+                                ListNode(2, ListNode(6))]), [
+                1, 1, 2, 3, 4, 4, 5, 6])
 
         def test_example_2(self):
-            self.assertEqual(f(), None)
+            self.assertEqual(f([]), [])
 
         def test_example_3(self):
-            self.assertEqual(f(), None)
+            self.assertEqual(f([ListNode()]), [])
 
     unittest.main()
-
-# Example 1:
-# Input: lists = [[1,4,5],[1,3,4],[2,6]]
-# Output: [1,1,2,3,4,4,5,6]
-# Explanation: The linked-lists are:
-# [
-#   1->4->5,
-#   1->3->4,
-#   2->6
-# ]
-# merging them into one sorted list:
-# 1->1->2->3->4->4->5->6
-
-# Example 2:
-# Input: lists = []
-# Output: []
-
-# Example 3:
-# Input: lists = [[]]
-# Output: []
