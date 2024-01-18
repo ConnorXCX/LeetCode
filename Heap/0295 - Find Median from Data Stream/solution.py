615941eb7801807d11f8cebeb363190b604fe202
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2024-01-18 11:52:55 -0500

    295 Solution.

--- Heap/0295 - Find Median from Data Stream/solution.py
+++ Heap/0295 - Find Median from Data Stream/solution.py
@@ -1,48 +1,56 @@
-class Solution:
-    # Time Complexity:  TBD
-    # Space Complexity: TBD
-    def solution(self) -> None:
-        pass
+from cgitb import small
+import heapq
+from operator import le
+
 
 class MedianFinder:
+    # Heap / Priority Queue implementation.
 
     def __init__(self):
-        
+        # Two Heaps -> maxHeap and minHeap.
+        # Both Heaps should be equal size.
+        self.maxHeap, self.minHeap = [], []
 
+    # O(logn) to add to or remove from Heap.
     def addNum(self, num: int) -> None:
-        
+        # maxHeap is not implemented by default in Python.
+        heapq.heappush(self.maxHeap, -1 * num)
 
+        # Make sure every num in maxHeap is <= every num in minHeap.
+        if (self.maxHeap and self.minHeap and (-1 * self.maxHeap[0]) > self.minHeap[0]):
+            val = -1 * heapq.heappop(self.maxHeap)
+            heapq.heappush(self.minHeap, val)
+
+        # Check for uneven size between the two Heaps.
+        if len(self.maxHeap) > len(self.minHeap) + 1:
+            val = -1 * heapq.heappop(self.maxHeap)
+            heapq.heappush(self.minHeap, val)
+
+        if len(self.minHeap) > len(self.maxHeap) + 1:
+            val = heapq.heappop(self.minHeap)
+            heapq.heappush(self.maxHeap, -1 * val)
+
+    # O(1) to get min or max of Heap.
     def findMedian(self) -> float:
+        if len(self.maxHeap) > len(self.minHeap):
+            return -1 * self.maxHeap[0]
+        if len(self.minHeap) > len(self.maxHeap):
+            return self.minHeap[0]
+
+        return (-1 * self.maxHeap[0] + self.minHeap[0]) / 2
 
 
 if __name__ == '__main__':
     import unittest
 
-    f = Solution().solution
-
-    # Your MedianFinder object will be instantiated and called as such:
-    # obj = MedianFinder()
-    # obj.addNum(num)
-    # param_2 = obj.findMedian()
-
     class Test(unittest.TestCase):
 
         def test_example_1(self):
-            self.assertEqual(f(), None)
+            medianFinder = MedianFinder()
+            medianFinder.addNum(1)
+            medianFinder.addNum(2)
+            medianFinder.findMedian()
+            medianFinder.addNum(3)
+            self.assertEqual(medianFinder.findMedian(), 2.0)
 
     unittest.main()
-
-# Example 1:
-# Input
-# ["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
-# [[], [1], [2], [], [3], []]
-# Output
-# [null, null, null, 1.5, null, 2.0]
-
-# Explanation
-# MedianFinder medianFinder = new MedianFinder();
-# medianFinder.addNum(1);    // arr = [1]
-# medianFinder.addNum(2);    // arr = [1, 2]
-# medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
-# medianFinder.addNum(3);    // arr[1, 2, 3]
-# medianFinder.findMedian(); // return 2.0
