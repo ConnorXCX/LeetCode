16aa29ab647a72d4b1e94e035bb2b3c96ff8983d
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2024-01-18 16:57:18 -0500

    102 Solution.

--- Tree/0102 - Binary Tree Level Order Traversal/solution.py
+++ Tree/0102 - Binary Tree Level Order Traversal/solution.py
@@ -1,4 +1,6 @@
+from collections import deque
 from typing import List, Optional
+from unittest import result
 
 
 # Definition for a binary tree node.
@@ -10,10 +12,30 @@ class TreeNode:
 
 
 class Solution:
-    # Time Complexity:  TBD
-    # Space Complexity: TBD
+    # Time Complexity:  O(n) - BFS implementation; visiting every node a single time.
+    # Space Complexity: O(n) - Levels of a Binary Tree have max of n/2.
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        return [[]]
+        res = []
+
+        q = deque()
+        q.append(root)
+
+        while q:
+            qLength = len(q)
+            level = []
+
+            for i in range(qLength):
+                node = q.popleft()
+
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+
+            if level:
+                res.append(level)
+
+        return res
 
 
 if __name__ == '__main__':
@@ -24,12 +46,13 @@ if __name__ == '__main__':
     class Test(unittest.TestCase):
 
         def test_example_1(self):
-            self.assertEqual(f(), None)
+            self.assertEqual(f(TreeNode(3, TreeNode(9), TreeNode(
+                20, TreeNode(15), TreeNode(7)))), [[3], [9, 20], [15, 7]])
 
         def test_example_2(self):
-            self.assertEqual(f(), None)
+            self.assertEqual(f(TreeNode(1)), [[1]])
 
         def test_example_3(self):
-            self.assertEqual(f(), None)
+            self.assertEqual(f(None), [])
 
     unittest.main()
