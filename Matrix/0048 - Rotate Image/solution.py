d2aa6fc14e496623c58ac9f8517cf6fad1ef5356
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2024-01-31 02:19:24 -0500

    48 Solution.

--- Matrix/0048 - Rotate Image/solution.py
+++ Matrix/0048 - Rotate Image/solution.py
@@ -2,10 +2,38 @@ from typing import List
 
 
 class Solution:
-    # Time Complexity:  TBD
-    # Space Complexity: TBD
+    # Time Complexity:  O(n^2) - iterating over matrix once.
+    # Space Complexity: O(1) - in-place operation, using four pointers / boundaries to track rotations.
     def rotate(self, matrix: List[List[int]]) -> List[List[int]]:
+        left, right = 0, len(matrix) - 1
 
+        while left < right:
+            for i in range(right - left):
+                # Same as left and right pointers / boundaries since given matrix is guaranteed to be n x n.
+                top, bottom = left, right
+
+                # Save top left value in temporary in-place variable.
+                topLeft = matrix[top][left + i]
+
+                # Move bottom left into top left.
+                matrix[top][left + i] = matrix[bottom - i][left]
+
+                # Move bottom right into bottom left.
+                matrix[bottom - i][left] = matrix[bottom][right - i]
+
+                # Move top right into bottom right.
+                matrix[bottom][right - i] = matrix[top + i][right]
+
+                # Move top left into top right.
+                matrix[top + i][right] = topLeft
+
+            # Shift right pointer / boundary left by one.
+            right -= 1
+
+            # Shift left pointer / boundary right by one.
+            left += 1
+
+        return matrix
 
 
 if __name__ == '__main__':
@@ -16,9 +44,11 @@ if __name__ == '__main__':
     class Test(unittest.TestCase):
 
         def test_example_1(self):
-            self.assertEqual(f([[1,2,3],[4,5,6],[7,8,9]]), [[7,4,1],[8,5,2],[9,6,3]])
+            self.assertEqual(f([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), [
+                             [7, 4, 1], [8, 5, 2], [9, 6, 3]])
 
         def test_example_2(self):
-            self.assertEqual(f([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]), [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]])
+            self.assertEqual(f([[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]), [
+                             [15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]])
 
     unittest.main()
--- README.md
+++ README.md
@@ -71,7 +71,7 @@ Collection of Python solutions to LeetCode Blind 75 questions.
 
 1. ✅ Set Matrix Zeroes
 2. ✅ Spiral Matrix
-3. 🟡 Rotate Image
+3. ✅ Rotate Image
 4. 🟡 Word Search
 
 **String**
