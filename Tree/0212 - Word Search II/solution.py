8d0ad4b7d52503304636df0a5aac325b91b93973
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2024-01-31 21:40:24 -0500

    212 Solution.

--- Tree/0212 - Word Search II/solution.py
+++ Tree/0212 - Word Search II/solution.py
@@ -1,24 +1,26 @@
+from typing import List
+
+
 class Solution:
     # Time Complexity:  TBD
     # Space Complexity: TBD
-    def solution(self) -> None:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        # Trie (Prefix Tree) Implementation to organize words based on their prefixes.
         pass
 
 
 if __name__ == '__main__':
     import unittest
 
-    f = Solution().solution
+    f = Solution().findWords
 
     class Test(unittest.TestCase):
 
         def test_example_1(self):
-            self.assertEqual(f(), None)
+            self.assertEqual(f([["o", "a", "a", "n"], ["e", "t", "a", "e"], ["i", "h", "k", "r"], [
+                             "i", "f", "l", "v"]], ["oath", "pea", "eat", "rain"]), ["eat", "oath"])
 
         def test_example_2(self):
-            self.assertEqual(f(), None)
-
-        def test_example_3(self):
-            self.assertEqual(f(), None)
+            self.assertEqual(f([["a", "b"], ["c", "d"]], ["abcb"]), [])
 
     unittest.main()
