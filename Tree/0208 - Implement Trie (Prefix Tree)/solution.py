1cd4016dbf5e7f587ac940c85a6441daac58a6e6
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2024-01-31 21:40:37 -0500

    208 Solution.

--- Tree/0208 - Implement Trie (Prefix Tree)/solution.py
+++ Tree/0208 - Implement Trie (Prefix Tree)/solution.py
@@ -1,24 +1,37 @@
-class Solution:
-    # Time Complexity:  TBD
-    # Space Complexity: TBD
-    def solution(self) -> None:
-        pass
+# Time Complexity:  TBD
+# Space Complexity: TBD
+class Trie:
 
+    def __init__(self):
+        
+
+    def insert(self, word: str) -> None:
+        
+
+    def search(self, word: str) -> bool:
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        
 
 if __name__ == '__main__':
     import unittest
 
-    f = Solution().solution
+    # f = Trie().solution
+    # Your Trie object will be instantiated and called as such:
+    # obj = Trie()
+    # obj.insert(word)
+    # param_2 = obj.search(word)
+    # param_3 = obj.startsWith(prefix)
 
     class Test(unittest.TestCase):
 
         def test_example_1(self):
-            self.assertEqual(f(), None)
-
-        def test_example_2(self):
-            self.assertEqual(f(), None)
-
-        def test_example_3(self):
+            # Input
+            # ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+            # [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+            # Output
+            # [null, null, true, false, true, null, true]
             self.assertEqual(f(), None)
 
     unittest.main()
