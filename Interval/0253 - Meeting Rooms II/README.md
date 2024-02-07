4fcb9ec0e5d64f1c52982adbd9ce3d4a18730cc7
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2024-02-06 21:35:41 -0500

    253 Solution.

--- Interval/0253 - Meeting Rooms II/README.md
+++ Interval/0253 - Meeting Rooms II/README.md
@@ -1,3 +1,3 @@
 # Meeting Rooms II
 
-Given an array of meeting time `intervals` intervals where <code>intervals[i] = [start<sub>i</sub>, end<sub>i</sub>]</code>, return _the minimum number of conference rooms required_.
+Given an array of meeting time `intervals` intervals where <code>intervals[i] = [start<sub>i</sub>, end<sub>i</sub>]</code>, return _the minimum number of conference rooms required (or maximum number of overlapping meetings at any given point in time)_.
--- Interval/0253 - Meeting Rooms II/solution.py
+++ Interval/0253 - Meeting Rooms II/solution.py
@@ -2,10 +2,25 @@ from typing import List
 
 
 class Solution:
-    # Time Complexity:  TBD
-    # Space Complexity: TBD
+    # Time Complexity:  O(nlogn) - need to sort intervals array.
+    # Space Complexity: O(n) - need a start time array and end time array to get max overlaps.
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        pass
+        startTimes = sorted([interval[0] for interval in intervals])
+        endTimes = sorted([interval[-1] for interval in intervals])
+
+        count, maxCount, startPointer, endPointer = 0, 0, 0, 0
+
+        while startPointer < len(intervals):
+            if startTimes[startPointer] < endTimes[endPointer]:
+                startPointer += 1
+                count += 1
+            else:
+                endPointer += 1
+                count -= 1
+
+            maxCount = max(maxCount, count)
+
+        return maxCount
 
 
 if __name__ == '__main__':
